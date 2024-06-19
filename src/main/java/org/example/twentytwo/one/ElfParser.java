908614730c8c599118f3e49633576285cf77e23d
Bethanie Garfin <bethanie.garfin@autotrader.co.uk>
2024-06-19 14:14:33 +0100

    Make delimiter a constant

--- src/main/java/org/example/twentytwo/one/ElfParser.java
+++ src/main/java/org/example/twentytwo/one/ElfParser.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ElfParser {
+    public static final String EMPTY_LINE_DELIMITER = "\\n\\n+";
 
     public ArrayList<Elf> parseCalorieFile(String filePath) throws FileNotFoundException {
         InputStream io = getClass().getClassLoader().getResourceAsStream(filePath);
@@ -14,19 +15,19 @@ public class ElfParser {
             throw new FileNotFoundException("File path not found");
         }
 
-        Scanner scanner = new Scanner(io).useDelimiter("\\n\\n+");
+        Scanner scanner = new Scanner(io).useDelimiter(EMPTY_LINE_DELIMITER);
         ArrayList<Elf> elves = new ArrayList<>();
 
         while (scanner.hasNext()) {
             String caloriesString = scanner.next();
-            Elf elf = new Elf(parseCalorieString(caloriesString));
+            Elf elf = new Elf(parseCaloriesFromString(caloriesString));
             elves.add(elf);
         }
 
         return elves;
     }
 
-    private ArrayList<Integer> parseCalorieString(String calorieString) {
+    private ArrayList<Integer> parseCaloriesFromString(String calorieString) {
         ArrayList<Integer> calories = new ArrayList<>();
         for (String calorie: calorieString.split("\n")) {
             calories.add(Integer.valueOf(calorie));
